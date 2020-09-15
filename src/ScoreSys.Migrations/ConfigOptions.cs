a75fda11a1adf4ea78ef2aec34b2b0958c7b5430
Luke Parker <lparkermg@outlook.com>
2020-09-15 20:23:30 +0100

    Added migrations.

---
+++ src/ScoreSys.Migrations/ConfigOptions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ScoreSys.Migrations
+{
+    public static class ConfigOptions
+    {
+        private static readonly IConfigurationRoot Config;
+        static ConfigOptions()
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("config.json", false, false);
+            Config = builder.Build();
+        }
+
+        public static string Views => Config["SqlViewConnectionString"];
+    }
+}
