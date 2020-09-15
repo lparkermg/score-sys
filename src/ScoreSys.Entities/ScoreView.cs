aa0017e58760a0c4841e488fdef15aff34672475
Luke Parker <lparkermg@outlook.com>
2020-09-15 19:49:46 +0100

    Initial project setup + started added EF stuff.

---
+++ src/ScoreSys.Entities/ScoreView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ScoreSys.Entities
+{
+    public sealed class ScoreView
+    {
+        public Guid Id { get; set; }
+
+        public Guid GameId { get; set; }
+
+        public string Name { get; set; }
+
+        public int Score { get; set; }
+
+        public DateTime PostedAt { get; set; }
+    }
+}
