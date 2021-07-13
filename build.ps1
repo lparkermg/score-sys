30c76d6d0cecf4e826ae137a79180143543743c1
Luke Parker <lparkermg@outlook.com>
2021-07-13 21:50:50 +0100

    Updated build script to also push to passed in image base.

--- build.ps1
+++ build.ps1
@@ -1,18 +1,39 @@
 param (
-    [string]$version = "0.0.0"
+    [string]$version = "0.0.0",
+    [string]$imageBase = "score-sys"
 )
 
+# TODO: Uncomment when logging in to AWS and running the returned docker command is sorted.
+#[string]$region = "eu-west-2"
+
 function BuildImages(){
-    Write-Host("Building API image");
-    & docker image build -t score-sys/api:$version -t score-sys/api:latest --force-rm -f ./build/api/Dockerfile .
+    Write-Host("Building images for version $version");
+    Write-Host("===============");
+    & docker image build -t $imageBase/api:$version -t $imageBase/api:latest --force-rm -f ./build/api/Dockerfile .
+    & docker image build -t $imageBase/worker:$version -t $imageBase/worker:latest --force-rm -f ./build/worker/Dockerfile .
+    & docker image build -t $imageBase/migrations:$version -t $imageBase/migrations:latest --force-rm -f ./build/migrations/Dockerfile .
+}
 
-    Write-Host("Building Worker image");
-    & docker image build -t score-sys/worker:$version -t score-sys/worker:latest --force-rm -f ./build/worker/Dockerfile .
+function PushImages(){
+    # Todo: Login to aws and docker.
+    # $(aws ecr get-login --region $region --no-include-email)
+    Write-Host("Pushing images");
+    Write-Host("==============");
 
-    Write-Host("Building Migrations image");
-    & docker image build -t score-sys/migrations:$version -t score-sys/migrations:latest --force-rm -f ./build/migrations/Dockerfile .
-} 
+    Write-Host("Versioned images:");
+    & docker push $imageBase/api:$version
+    & docker push $imageBase/worker:$version
+    & docker push $imageBase/migrations:$version
+
+    Write-Host("Latest images:");
+    & docker push $imageBase/api:latest
+    & docker push $imageBase/worker:latest
+    & docker push $imageBase/migrations:latest
+}
 
 BuildImages
-
-Write-Host("Build complete");
+Write-Host("Images built");
+if($imageBase -ne "score-sys"){
+    PushImages
+    Write-Host("Images pushed");
+}
